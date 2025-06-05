071c51d11ff7ba4a8cadb35694b6baec6e488d50
aayans314 <ashah28@colby.edu>
2025-06-05 03:13:42 -0400

    final commits for dirst day

---
+++ pkwiththeclipper/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BarberSite from './BarberSite';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarberSite />
+  </React.StrictMode>
+);
